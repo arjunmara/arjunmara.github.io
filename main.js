36c084378e2d246a1fc74f1210d5e19d0e241214
Arjun Marasini <arjun.marasini10@gmail.com>
2018-04-19 17:21:31 +0545

    with the friggin modal

--- main.js
+++ main.js
@@ -46,34 +46,66 @@ arrowRight.addEventListener('click', function() {
 start();
 
 // ------------------for Gallery----------------------------------
-const currentImg = document.querySelector('#currentImg');
-const imgs = document.querySelectorAll('.imgs img');
-const opacity = 0.4;
- imgs.forEach(img =>
-   img.addEventListener('click',imageClick)
- );
- // set first image
- imgs[0].style.opacity = opacity;
-function imageClick(e){
-  // reset opacity
-  imgs.forEach(img=>(img.style.opacity=1));
-  // change current image src to source of clicked images
-  currentImg.src=e.target.src;
-  // Add fade in class
-  currentImg.classList.add('fade-in');
-  // remove fadeIn class
-  setTimeout(()=>currentImg.classList.remove('fade-in'),500);
-  // change opacity
-  e.target.style.opacity = opacity;
-}
+// const currentImg = document.querySelector('#currentImg');
+// const imgs = document.querySelectorAll('.imgs img');
+// const opacity = 0.4;
+//  imgs.forEach(img =>
+//    img.addEventListener('click',imageClick)
+//  );
+//  // set first image
+//  imgs[0].style.opacity = "0.4";
+// function imageClick(e){
+//   // reset opacity
+//   imgs.forEach(img=>(img.style.opacity=1));
+//   // change current image src to source of clicked images
+//   currentImg.src=e.target.src;
+//   // Add fade in class
+//   currentImg.classList.add('fade-in');
+//   // remove fadeIn class
+//   setTimeout(()=>currentImg.classList.remove('fade-in'),500);
+//   // change opacity
+//   e.target.style.opacity = opacity;
+// }
 
-// for sticky nav
-const nav = document.getElementById("#menu");
-const topOfNav = nav.offsetTop;
+//
+// let ham = document.getElementById('ham');
+// let main = document.getElementById('main');
+// let menu = document.getElementById('side-menu')
+// let x = document.getElementById('x');
+// ham.addEventListener('click', openSlideMenu);
+// x.addEventListener('click', closeSlideMenu)
+// function openSlideMenu(){
+//   menu.style.width = '250px';
+//   // menu.style.transition = '3s all';
+//   main.style.marginLeft = "250px";
+// }
+// function closeSlideMenu(){
+//   menu.style.width = '0';
+//   // menu.style.transition = '3s all'
+//   main.style.marginLeft = "0";
+// }
 
+/**************************************************************
+// *****************for the modal*************************** */
+const newsContainer = document.querySelector('.news');
+newsContainer.addEventListener('click', e=>{
+  e.preventDefault()
 
-function stickyNav() {
-console.log(topOfNav);
-}
+// modal toggle
+ const modalToggle = e.target.closest('.my-btn');
+  // console.log(modalToggle);
+  if(!modalToggle) return;
+  const modal = document.getElementById('news-modal');
+  const closeButton = modal.querySelector('.close-Btn');
+  modal.classList.add('is-open')
 
-window.addEventListener('scroll',stickyNav);
+  closeButton.addEventListener('click', closeModal);
+  function closeModal(){
+    modal.classList.remove('is-open')
+  }
+})
+// const closeButton = modal.querySelector('.close-Btn');
+// closeButton.addEventListener('click', closeModal);
+// function closemodal(){
+//   modal.classList.remove('is-open');
+// }
